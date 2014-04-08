08562e7f7d72c81edc62e080e457f883b2a5f92d
William Casarin <will@casarin.me>
2014-04-07 23:41:19 -0700

    simplify

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
 var through = require('through');
 
 module.exports = function(n) {
-  var stream = through(write);
-
-  function write(datum) {
+  return through(function write(datum) {
     if (n === 0) this.queue(datum);
     else --n;
-  }
-
-  return stream;
+  });
 };
