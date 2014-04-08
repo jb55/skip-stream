23693a9903800149b352cb57fb73eebffb06eb33
William Casarin <will@casarin.me>
2014-04-07 23:39:57 -0700

    remove debug crap

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 
 var through = require('through');
-var debug = require('debug')('skip-stream')
 
 module.exports = function(n) {
   var stream = through(write);
 
   function write(datum) {
-    if (n === 0) {
-      debug("queueing %s", datum);
-      this.queue(datum);
-    } else {
-      debug("decrementing %s", --n);
-    }
+    if (n === 0) this.queue(datum);
+    else --n;
   }
 
   return stream;
-}
+};
